e4955528d3d2f6b1001ed2f807a8424f6d90b496
anyshpm <anyshpm@anyshpm.com>
2024-07-05 01:42:22 +0000

    Update ChatTTS to 0.1.1

--- simple.py
+++ simple.py
@@ -4,10 +4,10 @@ import torchaudio
 import torch
 
 chat = ChatTTS.Chat()
-chat.load_models(compile=False) # 设置为True以获得更快速度
+chat.load(compile=False) # 设置为True以获得更快速度
 
 texts = ["在这里输入你的文本",]
 
-wavs = chat.infer(texts, use_decoder=True)
+wavs = chat.infer(texts)
 
 torchaudio.save("output1.wav", torch.from_numpy(wavs[0]), 24000, format='wav')
